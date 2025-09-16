9024ee67a83e17157d3eda5f53ee4618f22b4fce
Majo <majouscanga07@gmail.com>
2025-09-16 14:19:57 -0600

    Sass-Gulp

--- app/encabezado.php
+++ app/encabezado.php
@@ -13,16 +13,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link rel="preload" href="css/style.css" as="style">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" href="media/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../build/css/app.css">
+    <link rel="icon" href="../src/img/logo.png" type="image/x-icon">
 
 <!-- Encabezado con logo y título -->
 <div class="navegador">
   <nav class="navbar px-3 border-bottom w-100">
     <div class="contenedor d-flex justify-content-between align-items-center">
-      <a class="navbar-brand d-flex align-items-center" href="index.php">
-        <img class="nav-logo" src="media/logo-sinfondo.png" alt="Logo">
+      <a class="navbar-brand d-flex align-items-center" href="home.php">
+        <img class="nav-logo" src="..src/img/logo-sinfondo.png" alt="Logo">
         <div>
           <div class="system-title">EPSINOM-035</div>
           <div class="system-subtitle">Evaluación psicosocial con base en la NOM-035</div>
@@ -55,7 +54,7 @@
   <div class="contenedor">
     <ul class="nav nav-tabs px-3">
       <li class="nav-item">
-        <a class="nav-link active" href="index.php">Inicio</a>
+        <a class="nav-link active" href="home.php">Inicio</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Cuestionarios</a>
@@ -70,42 +69,3 @@
   </div>
 </div>
 </head>
-<body>
-  <div class="blog-contenido">
-    <div class="blog-header">
-      <h2 class="titulo-blog">Conoce la importancia de la NOM-035</h2>
-    </div>
-    
-    <div class="blog-container">
-      <img src="media/img1.png" alt="img" class="img-blog">
-      
-      <div class="blog-text">
-        <p class="blog">
-          La Norma Oficial Mexicana <span class="subrayado">NOM-035-STPS-2018</span>, Factores de Riesgo Psicosocial en el Trabajo 
-          – Identificación, Análisis y Prevención, es una regulación que busca proteger la salud mental y el bienestar de los trabajadores en México. Establece la obligación de los centros de trabajo de identificar, 
-          analizar y prevenir factores de riesgo psicosocial como el estrés laboral, 
-          la violencia organizacional y las condiciones desfavorables en el entorno de trabajo.
-          <br><br>
-          Este cuestionario es una herramienta esencial dentro de la implementación de la NOM-035. 
-          Su objetivo es conocer la percepción de los trabajadores respecto a aspectos como la carga de trabajo, liderazgo, relaciones laborales y condiciones del puesto.
-          <br><br>
-          Al participar, se contribuye a:
-          <br>
-          • <span class="subrayado">Detectar riesgos psicosociales que puedan afectar la salud y el clima laboral.</span>
-          <br>
-          • <span class="subrayado">Promover un ambiente de trabajo sano y productivo, fortaleciendo la cultura organizacional.</span>
-          <br>
-          • <span class="subrayado">Cumplir con las disposiciones legales de la Secretaría del Trabajo y Previsión Social (STPS).</span>
-          <br>
-          Tu colaboración es confidencial y de gran importancia, ya que permitirá diseñar acciones y estrategias que beneficien tanto a los trabajadores como a la organización.
-        </p>
-        <button class="inicioTest" hre href="cuestionario.php">Iniciar Cuestionario</button>
-    </div>
-  </div>  
-
-</body>
-
-  <!-- Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
---
+++ app/home.php
@@ -0,0 +1,40 @@
+<?php require_once 'encabezado.php'; ?>
+<body>
+  <div class="blog-contenido">
+    <div class="blog-header">
+      <h2 class="titulo-blog">Conoce la importancia de la NOM-035</h2>
+    </div>
+    
+    <div class="blog-container">
+      <img src="..src/img/img1.png" alt="Logo" class="blog-logo">
+      
+      <div class="blog-text">
+        <p class="blog">
+          La Norma Oficial Mexicana <span class="subrayado">NOM-035-STPS-2018</span>, Factores de Riesgo Psicosocial en el Trabajo 
+          – Identificación, Análisis y Prevención, es una regulación que busca proteger la salud mental y el bienestar de los trabajadores en México. Establece la obligación de los centros de trabajo de identificar, 
+          analizar y prevenir factores de riesgo psicosocial como el estrés laboral, 
+          la violencia organizacional y las condiciones desfavorables en el entorno de trabajo.
+          <br><br>
+          Este cuestionario es una herramienta esencial dentro de la implementación de la NOM-035. 
+          Su objetivo es conocer la percepción de los trabajadores respecto a aspectos como la carga de trabajo, liderazgo, relaciones laborales y condiciones del puesto.
+          <br><br>
+          Al participar, se contribuye a:
+          <br>
+          • <span class="subrayado">Detectar riesgos psicosociales que puedan afectar la salud y el clima laboral.</span>
+          <br>
+          • <span class="subrayado">Promover un ambiente de trabajo sano y productivo, fortaleciendo la cultura organizacional.</span>
+          <br>
+          • <span class="subrayado">Cumplir con las disposiciones legales de la Secretaría del Trabajo y Previsión Social (STPS).</span>
+          <br>
+          Tu colaboración es confidencial y de gran importancia, ya que permitirá diseñar acciones y estrategias que beneficien tanto a los trabajadores como a la organización.
+        </p>
+        <button class="inicioTest" hre href="cuestionario.php">Iniciar Cuestionario</button>
+    </div>
+  </div>  
+
+</body>
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
